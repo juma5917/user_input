fa377deb7e989e8fd1abe0121557cfa53a97528f
otieno <otieno@gmail.com>
2024-02-23 23:32:39 +0300

    Adding user_input.py file

---
+++ user_input.py
@@ -0,0 +1,8 @@
+
+name = input("What is your name? ")
+
+age = input("How old are you? ")
+
+location = input("Where are you located? ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
